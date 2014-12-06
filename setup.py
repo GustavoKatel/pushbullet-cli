e4d03a463fd71c1a75bee2ac07c8aea75fa153a0
Roey Darwish Dror <roey@toothless.local>
2014-12-06 17:28:34 +0200

    Use the Pushbullet library instead of requests

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name = "Pushbullet CLI",
     version = "0.1",
     packages = find_packages(exclude=['tests']),
-    install_requires=['requests'],
+    install_requires=['pushbullet.py'],
     entry_points = {
         'console_scripts': ['pb = pushbullet_cli.app:main']
     },
