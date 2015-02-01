8053f508fdb3f9bda9290b61ad8ac40f32947b47
Roey Darwish Dror <roey.ghost@gmail.com>
2015-02-01 10:19:12 +0200

    Add constranit for pushbullet.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name = "Pushbullet CLI",
     version = "0.1",
     packages = find_packages(exclude=['tests']),
-    install_requires=['pushbullet.py >= 0.7.1'],
+    install_requires=['pushbullet.py >= 0.7.1, < 0.8'],
     entry_points = {
         'console_scripts': ['pb = pushbullet_cli.app:main']
     },
