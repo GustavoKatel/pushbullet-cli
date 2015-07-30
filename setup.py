39f7d507e52790c9ce5731fe90567cb09edea57b
Roey Darwish Dror <roeyd@infinidat.com>
2015-07-30 09:46:30 +0300

    Move to click instead of argparse

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     url='https://github.com/r-darwish/pushbullet-cli',
     version="0.1",
     packages=find_packages(exclude=['tests']),
-    install_requires=['pushbullet.py >= 0.8.1'],
+    install_requires=['pushbullet.py >= 0.8.1', 'click'],
     entry_points={
         'console_scripts': ['pb = pushbullet_cli.app:main']
     },
