3490792800c3b0e4d9c376aa55f918e456de7c40
Gustavo Sampaio <gsampaio@esss.com.br>
2018-04-06 14:47:23 -0300

    Update pushbullet.py version. This fixes #33

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     url='https://github.com/GustavoKatel/pushbullet-cli',
     version='0.7.6',
     packages=find_packages(exclude=['tests']),
-    install_requires=['pushbullet.py >= 0.9.0', 'click', 'keyring>=8.2', 'keyrings.alt'],
+    install_requires=['pushbullet.py >= 0.11.0', 'click', 'keyring>=8.2', 'keyrings.alt'],
     entry_points={
         'console_scripts': ['pb = pushbullet_cli.app:main']
     },
