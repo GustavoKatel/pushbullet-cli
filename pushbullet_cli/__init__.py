a05446485670e5c6ac6a7175aa3e0b4a66399072
Gustavo Sampaio <gbritosampaio@gmail.com>
2018-10-23 16:18:26 -0300

    Fix python 2.7 compat

--- pushbullet_cli/__init__.py
+++ pushbullet_cli/__init__.py
@@ -1,1 +1,3 @@
+from __future__ import print_function, unicode_literals
+
 __author__ = 'GustavoKatel'
--- pushbullet_cli/__version__.py
+++ pushbullet_cli/__version__.py
@@ -1,1 +1,3 @@
+from __future__ import print_function, unicode_literals
+
 __version__ = '0.7.6'
